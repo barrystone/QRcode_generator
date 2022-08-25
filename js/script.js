a062d3b8cad86bbaa7b7f77060092b7438f9af15
barrystone <barrystone3183@gmail.com>
2022-08-25 14:55:52 +0800

    [feat] create qrcode& download features

--- js/script.js
+++ js/script.js
@@ -5,7 +5,12 @@ const showSpinner = () =>
   (document.getElementById('spinner').style.display = 'block');
 const hideSpinner = () =>
   (document.getElementById('spinner').style.display = 'none');
-const clearUI = () => (qr.innerHTML = '');
+
+const clearUI = () => {
+  qr.innerHTML = '';
+  const saveLink = document.getElementById('save-link');
+  if (saveLink) saveLink.remove();
+};
 
 const onGenerateSubmit = (e) => {
   e.preventDefault();
@@ -21,6 +26,10 @@ const onGenerateSubmit = (e) => {
     setTimeout(() => {
       hideSpinner();
       generateQRCode(url, size);
+      setTimeout(() => {
+        const saveUrl = qr.querySelector('img').src;
+        createSaveBtn(saveUrl);
+      }, 50);
     }, 1000);
   }
 };
@@ -32,6 +41,17 @@ const generateQRCode = (urlParams, sizeParams) => {
     width: sizeParams,
     height: sizeParams
   });
+};
+
+const createSaveBtn = (saveUrl) => {
+  const link = document.createElement('a');
+  link.id = 'save-link';
+  link.classList =
+    'bg-green-500 hover:bg-green-700 text-white font-bold py-2 rounded w-1/3 m-auto my-5';
+  link.href = saveUrl;
+  link.download = 'qrcode';
+  link.innerHTML = '儲存照片 <br /> (Save image)';
+  document.getElementById('generated').appendChild(link);
 };
 
 hideSpinner();
